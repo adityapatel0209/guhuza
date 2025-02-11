18b11eed72ace96a6ea1afce587e607426a4c5aa
srmagar2002 <167953839+srmagar2002@users.noreply.github.com>
2025-02-11 13:39:21 -0500

    Minor changes

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -80,7 +80,10 @@ const Sidebar: React.FC<SidebarProps> = ({
               >
                 <span className="question">Q{index + 1}: {question.question}</span>
                 {attemptedQuestions.includes(index) && (
-                  <span className="attempted"><img src={checksvg}/></span>
+                  <span className="attempted"><svg width="2rem" height="2rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path className="st0" d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#fff" stroke-width="2" stroke-linecap="round"
+                      stroke-linejoin="round" />
+              </svg></span>
                 )}
               </li>
             ))}
